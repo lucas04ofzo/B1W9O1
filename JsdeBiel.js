0acc12a50f0378cc6e58f3130f3dcde6fee10aa4
lucas04ofzo <tigelaarl04@gmail.com>
2021-01-29 12:41:14 +0100

    begin opdr

--- JsdeBiel.js
+++ JsdeBiel.js
@@ -1,26 +1,43 @@
 var vraag
 var drink
-var aantal
+var fris
+var bier
+var wijn
 var stop
+var fout
+var aantalfris
+var aantalbier
+var aantalwijn
 function drinken(){
 	vraag=prompt('Welke bestelling wilt u toevoegen? Kies uit: fris, bier of wijn. Typ stop als u de rekening wilt.');
 	drink=vraag.toLowerCase();
 	if(drink=='fris'){
-		alert('fris');
+		fris=1
 	}else if(drink=='bier'){
-		alert('bier');
+		bier=1
 	}else if(drink=='wijn'){
-		alert('wijn');
+		wijn=1
 	}else if(drink=='stop'){
 		alert('stop');
-		stop++
 	}else{
 		alert('U heeft een ongeldige invoer gedaan. Uw bestelling kan niet worden toegevoegd.');
 	}
-}function bestellen(){
-	aantal=prompt('Hoeveel '+drink+' wilt u toevoegen aan uw bestelling?');
-	alert('U heeft '+aantal+' '+drink+' besteld');
-}do{
+}
+function bestellen(){
+	if(fris=1){
+		aantalfris=prompt('Hoeveel fris wilt u toevoegen aan uw bestelling?');
+	}alert('U heeft '+aantalfris+' fris besteld');
+	if(bier=1){
+		aantalbier=prompt('Hoeveel bier wilt u toevoegen aan uw bestelling?');
+	}alert('U heeft '+aantalbier+' bier besteld');
+	if(wijn=1){
+		aantalwijn=prompt('Hoeveel wijn wilt u toevoegen aan uw bestelling?');
+	}alert('U heeft '+aantalwijn+' wijn besteld');
+}
+if(fout=1){
+	drinken();
+}
+do{
 	drinken();
 }while(stop!='stop'){
 	bestellen();
